eda58bd1858f87efa40221d7436bbd9e08e1afcd
Nima <87241816+nimacpp@users.noreply.github.com>
2022-02-22 19:56:29 +0330

    1.2.0

--- ip.rb
+++ ip.rb
@@ -1,4 +1,6 @@
-require "httparty"
+#! /usr/bin/env ruby
+
+require "net/http"
 require "json"
 class String
 def red;            "\e[31m#{self}\e[0m" end
@@ -9,23 +11,43 @@ def magenta;        "\e[35m#{self}\e[0m" end
 def cyan;           "\e[36m#{self}\e[0m" end
 def gray;           "\e[37m#{self}\e[0m" end
 end
-system("cls")
-puts '''
- --------  -----------       --------  ----    ---- ------------   --------   
- ********  ************      ********  *****   **** ************  **********  
-   ----    ---      ---        ----    ------  ---- ----         ----    ---- 
-   ****    ************        ****    ************ ************ ***      *** 
-   ----    -----------         ----    ------------ ------------ ---      --- 
-   ****    ****                ****    ****  ****** ****         ****    **** 
- --------  ----              --------  ----   ----- ----          ----------  
- ********  ****              ********  ****    **** ****           ******** '''.green
+if nil != ARGV[0] 
+  ip = ARGV[0] 
+  p ip
+  url = URI.parse("http://ip-api.com/json/#{ip}")
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+  }
+  re1 = JSON.parse(res.body)
+  re1.each_pair do |shark, index|
+  puts "[+] ".green+"#{shark} ".magenta+"=> #{index}".cyan 
+  sleep 0.2
+  end
+else
+  system("cls")
+  puts '''
+  --------  -----------       --------  ----    ---- ------------   --------   
+  ********  ************      ********  *****   **** ************  **********  
+    ----    ---      ---        ----    ------  ---- ----         ----    ---- 
+    ****    ************        ****    ************ ************ ***      *** 
+    ----    -----------         ----    ------------ ------------ ---      --- 
+    ****    ****                ****    ****  ****** ****         ****    **** 
+  --------  ----              --------  ----   ----- ----          ----------  
+  ********  ****              ********  ****    **** ****           ******** '''.cyan
 
-print "\n Enter Your ip Target : ".brown
-ip = gets.chomp
-re = HTTParty.get("http://ip-api.com/json/#{ip}")
-re1 = JSON.parse(re.body)
-re1.each_pair do |shark, index|
-puts "[+] ".green+"#{shark} ".magenta+"=> #{index}".cyan 
-sleep 0.2
-end
-                                                                             
+  print "\n Enter Your ip Target : ".green
+  ip = $stdin.gets.chomp
+  
+  url = URI.parse("http://ip-api.com/json/#{ip}")
+  req = Net::HTTP::Get.new(url.to_s)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+  }
+
+  re1 = JSON.parse(res.body)
+  re1.each_pair do |shark, index|
+  puts "[+] ".green+"#{shark} ".magenta+"=> #{index}".cyan 
+  sleep 0.2
+  end
+end                                                                       
